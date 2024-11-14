3bf945cf9d8f12c5203f2654c1ac496a38baecd0
roblesdotdev <httpsnip@gmail.com>
2024-11-14 18:33:47 -0300

    setup flat routes

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
   server: {
     port: 3000,
@@ -20,6 +14,8 @@ export default defineConfig({
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+        unstable_routeConfig: true,
+        unstable_optimizeDeps: true,
       },
     }),
     tsconfigPaths(),
