c4bfa32c970e85e10df049e2e2ddac3c10d880aa
roblesdotdev <httpsnip@gmail.com>
2024-11-14 23:28:16 -0300

    setup dev tools & config stuff

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,23 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { remixDevTools } from 'remix-development-tools'
+
+const MODE = process.env.NODE_ENV
+const IS_PROD = MODE === 'production'
 
 export default defineConfig({
+  build: {
+    cssMinify: IS_PROD,
+    sourcemap: true,
+  },
   server: {
     port: 3000,
   },
   plugins: [
+    remixDevTools({ suppressDeprecationWarning: true }),
     remix({
+      serverModuleFormat: 'esm',
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
