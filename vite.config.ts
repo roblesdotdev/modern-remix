c4d50907fb38360a1bf297b1a3f6a0fc6e2b3c8e
roblesdotdev <httpsnip@gmail.com>
2024-11-14 23:44:10 -0300

    migrate to eslint 9

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from '@remix-run/dev'
+import { remixDevTools } from 'remix-development-tools'
 import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { remixDevTools } from 'remix-development-tools'
 
 const MODE = process.env.NODE_ENV
 const IS_PROD = MODE === 'production'
